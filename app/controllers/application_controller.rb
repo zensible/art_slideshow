87c5751e7b497df1b4f91054ac758d1fa2b75f50
eightfold <eightfold@gears.local>
2017-03-31 20:34:52 -0500

    Fancy new favicon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_filter :set_metadata
+
+  def set_metadata
+    @description = "Art Slideshow"
+    @keywords = "art, slideshow, met, the met, Metropolitan Museum, museum"
+    @pagename = "Art Slideshow"
+    @image = "#{request.protocol}#{request.host_with_port}/arcade/arcade_preview.jpg"
+    @sitename = "jsante.net"  
+  end
+
 end
