70074b67e07621c82f4a606b824e478a57b7506f
eightfold <eightfold@gears.local>
2017-03-31 21:12:38 -0500

    More tweaks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ class ApplicationController < ActionController::Base
   before_filter :set_metadata
 
   def set_metadata
-    @description = "A slideshow of the more than 200,000 public domain images released by the Metropolitan Museum of New York"
-    @keywords = "art, slideshow, met, the met, Metropolitan Museum, museum, paintings, sculpture, asian, american, modern art, egyptian art, arms, armor, medieval"
-    @pagename = "Fine Art Slideshow - 186,000 public domain works of art at random"
     @image = "#{request.protocol}#{request.host_with_port}/social.jpg"
-    @sitename = "jsante.net"  
+    @pagename = "Fine Art Slideshow"
+    @description = "A high-quality randomized slideshow of fine art from the Metropolitan Museum's more than 200,000 public domain works, including paintings, sculpture, drawings, photographs and more. Perfect for digital picture frames and just generally basking in the splendor of human artistic achievement."
+    @keywords = "fine art, slideshow, met, the met, Metropolitan Museum, museum, paintings, sculpture, asian, american, modern art, egyptian art, arms, armor, medieval"
+    @sitename = "art-slideshow.net"
   end
 
 end
