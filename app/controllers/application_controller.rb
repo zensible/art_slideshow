dbc699f53dc52b5cdb22b984856c17194746f1b5
eightfold <eightfold@gears.local>
2017-03-31 20:44:09 -0500

    Fix facebook hookup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,10 @@ class ApplicationController < ActionController::Base
   before_filter :set_metadata
 
   def set_metadata
-    @description = "Art Slideshow"
+    @description = "A slideshow of the more than 200,000 public domain images released by the Metropolitan Museum of New York"
     @keywords = "art, slideshow, met, the met, Metropolitan Museum, museum"
     @pagename = "Art Slideshow"
-    @image = "#{request.protocol}#{request.host_with_port}/arcade/arcade_preview.jpg"
+    @image = "#{request.protocol}#{request.host_with_port}/social.jpg"
     @sitename = "jsante.net"  
   end
 
